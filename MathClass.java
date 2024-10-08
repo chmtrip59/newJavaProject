44e11727b162d68a5f8f91924216461d8bf9ea55
chmtrip59 <chmtrip59@gmail.com>
2024-10-08 21:14:25 +0530

    Math Class

---
+++ MathClass.java
@@ -0,0 +1,28 @@
+public class MathClass{
+    public static void main(String[] args) {
+        /* 
+        //Math.abs()
+        System.out.println(Math.abs(-99));
+
+        //Math.ceil()
+        System.out.println(Math.ceil(5.07));
+        //Math.floor
+        System.out.println(Math.floor(5.07));
+        //Math.round
+        System.out.println(Math.round(5.07));
+
+        //Math.random()
+        System.out.println(Math.random());
+        */
+        for(int i = 0; i < 10;i++){
+            // System.out.println(Math.random());
+            // System.out.println(Math.random()*3);
+            // System.out.println(Math.round(Math.random()*100));
+
+
+        }
+
+        //Math.PI
+        System.out.println(Math.PI);
+    }
+}   
