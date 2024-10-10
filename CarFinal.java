0e659a783f9422898aae84ad3507870e792f2e09
chmtrip59 <chmtrip59@gmail.com>
2024-10-10 13:38:16 +0530

    Using final

---
+++ CarFinal.java
@@ -0,0 +1,20 @@
+public class CarFinal {
+    final int noOfWheels;
+
+    final String model;
+
+    final String engineInLiter;
+
+    CarFinal(int noOfWheels,String model,String engineInLiter){
+
+        this.noOfWheels =noOfWheels;
+
+        this.model = model;
+
+        this.engineInLiter =engineInLiter;
+
+        
+
+    }
+
+}
