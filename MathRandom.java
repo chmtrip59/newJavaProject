5d6b9642a87ef960012f74fa040e2a519961ade2
chmtrip59 <chmtrip59@gmail.com>
2024-10-10 11:27:39 +0530

    Math.random

--- MathRandom.java
+++ MathRandom.java
@@ -1,3 +1,18 @@
 public class MathRandom {
     
+    int roll(){
+        return (int) Math.ceil(Math.random() * 6);
+        
+
+        
+    }
+    
+    public static void main(String[] args) {
+        MathRandom dice = new MathRandom();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(dice.roll());
+        }
+    }
 }
+
